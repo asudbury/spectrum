df9e1f963f4b38f4a4f8bcf238f523d846b1c4b0
Adrian Sudbury <sudburya@gmail.com>
2017-02-06 16:57:41 +0000

    Page Guid works.

---
+++ Spectrum.Content/Services/IPageService.cs
@@ -0,0 +1,10 @@
+﻿namespace Spectrum.Content.Services
+{
+    public interface IPageService
+    {
+        /// <summary>
+        /// Gets the registration complete page.
+        /// </summary>
+        string RegistrationCompletePage { get; }
+    }
+}
