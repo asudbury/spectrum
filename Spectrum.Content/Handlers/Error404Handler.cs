a1296fb3495424bb56e49b8dfa6657eba985e4ba
Adrian Sudbury <sudburya@gmail.com>
2017-06-09 20:03:01 +0100

    404 Error page handling.

---
+++ Spectrum.Content/Handlers/Error404Handler.cs
@@ -0,0 +1,31 @@
+﻿namespace Spectrum.Content.Handlers
+{
+    using System.Linq;
+    using Umbraco.Core.Models;
+    using Umbraco.Web.Routing;
+
+    public class Error404Handler : IContentFinder
+    {
+        /// <summary>
+        /// Tries to find and assign an Umbraco document to a <c>PublishedContentRequest</c>.
+        /// </summary>
+        /// <param name="contentRequest">The <c>PublishedContentRequest</c>.</param>
+        /// <returns>A value indicating whether an Umbraco document was found and assigned.</returns>
+        /// <remarks>Optionally, can also assign the template or anything else on the document request, although that is not required.</remarks>
+        public bool TryFindContent(PublishedContentRequest contentRequest)
+        {
+            if (contentRequest.PublishedContent == null)
+            {
+                IPublishedContent home = contentRequest.RoutingContext.UmbracoContext.ContentCache.GetAtRoot().First();
+
+                IPublishedContent notFoundNode = home.Children.Single(x => x.Name == "Error-404");
+
+                contentRequest.SetResponseStatus(404, "404 Page Not Found");
+                
+                contentRequest.PublishedContent = notFoundNode;
+            }
+
+            return contentRequest.PublishedContent != null;
+        }
+    }
+}
---
+++ Spectrum.Umbraco/MyApplication.cs
@@ -0,0 +1,21 @@
+﻿using Umbraco.Core;
+using Umbraco.Web.Routing;
+
+namespace Spectrum.Umbraco
+{
+    using Content.Handlers;
+
+    public class MyApplication : ApplicationEventHandler
+    {
+        /// <summary>
+        /// Overridable method to execute when All resolvers have been initialized but resolution is not frozen so they can be modified in this method
+        /// </summary>
+        /// <param name="umbracoApplication"></param>
+        /// <param name="applicationContext"></param>
+        protected override void ApplicationStarting(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+        {
+            ContentFinderResolver.Current.InsertTypeBefore<ContentFinderByNotFoundHandlers, Error404Handler>();
+            base.ApplicationStarting(umbracoApplication, applicationContext);
+        }
+    }
+}
