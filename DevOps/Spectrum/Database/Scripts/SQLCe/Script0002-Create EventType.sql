2cf5acd4964d2bce2e7984d76f887aa6d34c49c2
Adrian Sudbury <sudburya@gmail.com>
2017-08-22 19:58:02 +0100

    more tidying up!!!

--- DevOps/Spectrum/Database/Scripts/SQLCe/Script0002-Create EventType.sql
+++
@@ -1,4 +0,0 @@
-﻿CREATE TABLE [EventType](
-	[Id] [int] NOT NULL PRIMARY KEY,
-	[Description] [Nvarchar](100) NOT NULL
- )
--- DevOps/Spectrum/Database/Scripts/SQLCe/Script0003-Create Event.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [Event](
-	[Guid] [uniqueidentifier] NOT NULL PRIMARY KEY,
-	[Event] [int] NOT NULL,
-	[DateTime] [datetime] NOT NULL DEFAULT (getdate()),
-	[Description] [nvarchar](100),
-	[Text] [nvarchar](100),
-	FOREIGN KEY (Event) REFERENCES EventType(Id) ON DELETE CASCADE ON UPDATE CASCADE
- )
