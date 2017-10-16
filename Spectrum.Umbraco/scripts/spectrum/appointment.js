04e847f4357cd0ff6dc51c2a45a0d5e79647c7fe
r4063 <r4063@HP20034682>
2017-10-16 22:10:54 +0100

    Attendee mandatory and appointent JS now common.

--- Spectrum.Umbraco/scripts/spectrum/appointment.js
+++ Spectrum.Umbraco/scripts/spectrum/appointment.js
@@ -1,8 +1,19 @@
 ﻿/* Supporting code for insert and maintain appointments */
 
-function updateAttendees() {
+function checkAttendees() {
 
-    console.log('running update attendees');
+    var items = document.getElementById("attendeeList").getElementsByTagName("li");
+
+    if (items.length === 0) {
+        event.preventDefault();
+        $('#attendeeInput').parents(".form-group").addClass('has-error');
+        $('#attendeeInput').addClass('text-danger');
+        $('span[data-valmsg-for="attendeeInput"]').text('Please enter at least one attendee');
+        $('span[data-valmsg-for="attendeeInput"]').addClass('text-danger');
+    }
+}
+
+function updateAttendees() {
 
     var items = document.getElementById("attendeeList").getElementsByTagName("li");
 
@@ -27,14 +38,36 @@ function prependAttendee(index, inputValue) {
     return (retVal);
 }
 
-
-
 function addErrorFields() {
 
     $('#attendeeInput').parents(".form-group").addClass('has-error');
     $('#attendeeInput').addClass('text-danger');
-    $('span[data-valmsg-for="attendeeInput"]').text('Please enter a valid xx Email Address');
+    $('span[data-valmsg-for="attendeeInput"]').text('Please enter a valid Email Address');
     $('span[data-valmsg-for="attendeeInput"]').addClass('text-danger');
+}
+
+function addAttendee() {
+
+    event.preventDefault();
+
+    var inputValue = $('#attendeeInput').val();
+    //This regex string stopped working when ported here from cshtml. But the replacement works fine.
+    //var filter = /^([a-zA-Z0-9_\.\-])+\@@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    var filter = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    if (!filter.test(inputValue)) {
+        addErrorFields();
+    } else {
+
+        var items = document.getElementById("attendeeList").getElementsByTagName("li");
+        var index = items.length;
+
+        $('#attendeeList').prepend(prependAttendee(index, inputValue));
+
+        $('#attendeeInput').val('');
+        $('span[data-valmsg-for="attendeeInput"]').text('');
+    }
+
 }
 
 function initialiseDatePicker() {
